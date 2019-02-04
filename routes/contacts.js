c7cd9c5d9665e6c230cf71235afeb1e88e23353b
martinpower <power.martin@outlook.com>
2019-02-04 12:21:38 -0500

    the name isn't hardcoded anymore. yay!

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+User = require('./schemas.js');
+
+const mongo = require("./mongo.json");
+const url = mongo.ConnectionString;
 
 /* GET contact manager page for logged in user. */
 router.get('/', function(req, res, next) {
     // TODO: pass user data from session to render
     // pass whole mongo object?
     // also need to pass contacts
-    res.render('contacts', {
-        name: "Autumn"
-    });
+	mongoose.connect(url, (err) => {
+		if(err) {
+			mongoose.disconnect();
+			throw err;
+		}
+	
+		User.findOne({ 
+			_id: req.session.uid
+		}, (err, user) => {
+			if(err) {
+				mongoose.disconnect();
+				throw err;
+			}
+
+			mongoose.disconnect();
+			res.render('contacts', {
+				name: user.name
+			});
+		});
+	});
 });
 
 module.exports = router;
