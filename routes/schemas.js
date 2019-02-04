62dc6b4a999c98b13ad6ca3472e44a150fcb457e
martinpower <power.martin@outlook.com>
2019-02-03 19:42:08 -0500

    added comparePassword to User schema

--- routes/schemas.js
+++ routes/schemas.js
@@ -16,5 +16,12 @@ var UserSchema = new mongoose.Schema({
   contacts: [ContactsSchema]
 });
 
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 mongoose.model('User', UserSchema);
 module.exports = mongoose.model('User');
